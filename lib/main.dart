778a06d97fae1a9c915a19a6e190a92a9520147c
Misterio <Misterriobit@gmail.com>
2023-05-31 12:08:44 +0700

    Init game

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
     runApp(const MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Center(
-          child: Text('Game'),
+          child: Text('Game!'),
         )));
   });
 }
